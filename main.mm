64386dcf1e43dc849b38a396b27381984c132b00
Max <mx@icj.me>
2016-11-22 10:15:36 -0500

    Initial code.

--- main.mm
+++ main.mm
@@ -2,3 +2,20 @@
  *
  * Yes.
 */
+
+#include "llvm/Object/MachO.h"
+#include "llvm/Object/MachOUniversal.h"
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		printf("Not enough params :(");
+		return 1;
+	}
+
+	printf("%s", argv[1]);
+	std::string fileName = std::string(argv[1]);
+	
+	llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> BufferOrErr = llvm::MemoryBuffer::getFileOrSTDIN(fileName);
+
+	return 0;
+}

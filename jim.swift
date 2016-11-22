146947f90d1aed743ef4f558df7ff0bdcb614808
Max <mx@icj.me>
2016-11-22 10:33:21 -0500

    Added example code from Jim

---
+++ jim.swift
@@ -0,0 +1,89 @@
+#!/usr/bin/swift
+import Foundation
+
+class State {
+	
+	init(_ affairs:[String:AnyObject]) {
+		self.💩 = affairs
+	}
+	
+	func mutate(_ like:(([String:AnyObject]) -> [String:AnyObject])) -> State {
+		return State(like(self.💩))
+	}
+	
+	var description:String {
+		return 💩.description
+	}
+	
+	private
+	let 💩:[String:AnyObject]
+}
+
+protocol Aggitatable {
+	var state:State { get set }
+}
+
+class Aggitator {
+	
+	static let attributes:[String] = ["FartCapacitance", "ElectrostaticReactance", "ElectrostaticResistance", "HealthPoints"]
+	
+	var aggitatees:[Aggitatable]
+	
+	init(_ aggitatee: Aggitatable) {
+		self.aggitatees = [aggitatee]
+	}
+	
+	init(_ aggitatees: [Aggitatable]) {
+		self.aggitatees = aggitatees
+	}
+	
+	func aggitate(_ like:((Aggitatable) -> Aggitatable)) -> [Aggitatable] {
+		for (index, _) in aggitatees.enumerated() {
+			aggitatees[index] = like(aggitatees[index])
+		}
+		return aggitatees
+	}
+}
+
+class Max : Aggitatable, CustomStringConvertible {
+	var state:State
+	
+	var description:String {
+		return self.state.description
+	}
+	
+	required init(_ state:State = State([:])) {
+		self.state = state
+	}
+}
+
+func main() {
+	var max = Max()
+	let agg = Aggitator(max)
+	
+	let 😂 = { (aggitatee: Aggitatable) -> Aggitatable in
+		if let max = aggitatee as? Max {
+			return type(of:max).init(max.state.mutate() { (before) in
+				var after = before
+				let random = Int(arc4random_uniform(UInt32(Aggitator.attributes.count)))
+				if random >= before.count {
+					after[Aggitator.attributes[random]] = arc4random_uniform(100) as AnyObject
+				} else if random < before.count {
+					after[Aggitator.attributes[random]] = nil
+				}
+				return after
+			})
+		} else {
+			return aggitatee
+		}
+	}
+	
+	print(max)
+	let 🐸s = agg.aggitate(😂)
+	if let 🐸 = 🐸s[0] as? Max {
+		max = 🐸
+	}
+	print(max)
+}
+
+main()
--- main.mm
+++ main.mm
@@ -6,6 +6,9 @@
 #include "llvm/Object/MachO.h"
 #include "llvm/Object/MachOUniversal.h"
 
+using namespace llvm;
+using namespace object;
+
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		printf("Not enough params :(");
@@ -15,9 +18,18 @@ int main(int argc, char **argv) {
 	printf("%s", argv[1]);
 	std::string fileName = std::string(argv[1]);
 	
-	llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> BufferOrErr = llvm::MemoryBuffer::getFileOrSTDIN(fileName);
+	llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> bufferOrError = llvm::MemoryBuffer::getFileOrSTDIN(fileName);
 	
-	
+//	if (error(bufferOrError.getError(), fileName))
+//		return 1;
 
+	
+	auto binOrError = createBinary(bufferOrError.get()->getMemBufferRef(), nullptr);
+	
+	if (!binOrError) {
+		printf("Error!\r\n");
+		return 1;
+	}
+	
 	return 0;
 }

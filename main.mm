6a9887669fbef13af3ff8b38db3164c92c52ab66
Max <mx@icj.me>
2016-11-22 11:06:31 -0500

    Work on printing mach-o header infos.

--- main.mm
+++ main.mm
@@ -9,13 +9,51 @@
 using namespace llvm;
 using namespace object;
 
+void printMachoInformation(MachOObjectFile *obj) {
+	// TODO: clean up this mess and finish it. Should just print relevant header-infos
+	
+	/* TODO: print should look like this:
+	 * CPUType: 0x1000007 (x86-64)
+	 * CPUType: 0x7 (x86)
+	 * CPUType: 0x12 (Arm)
+	 * CPUType: 0x1000012 (Arm64)
+	 * et al
+	 */
+	
+	MachO::mach_header header;
+	MachO::mach_header_64 header64;
+	
+	Triple T;
+	if (obj->is64Bit()) {
+		printf("64bit!\r\n");
+		if (header64.cpusubtype | llvm::MachO::CPU_TYPE_X86) {
+			printf("X86 found\r\n");
+		}
+		if (header64.cpusubtype | llvm::MachO::CPU_ARCH_ABI64) {
+			// confirms 64bit
+		}
+	
+		
+		header64 = obj->MachOObjectFile::getHeader64();
+		T = MachOObjectFile::getArchTriple(header64.cputype, header64.cpusubtype);
+		
+			printf("CPUType: 0x%x\r\nCPUSubtype: 0x%x", header64.cputype, header.cpusubtype);
+	}
+	else {
+		printf("32bit!\r\n");
+		header = obj->MachOObjectFile::getHeader();
+		T = MachOObjectFile::getArchTriple(header.cputype, header64.cpusubtype);
+		
+		printf("CPUType: 0x%x\r\nCPUSubtype: 0x%x", header64.cputype, header.cpusubtype);
+	}
+}
+
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		printf("Not enough params :(");
 		return 1;
 	}
 
-	printf("%s", argv[1]);
 	std::string fileName = std::string(argv[1]);
 	
 	llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> bufferOrError = llvm::MemoryBuffer::getFileOrSTDIN(fileName);
@@ -29,6 +67,32 @@ int main(int argc, char **argv) {
 	if (!binOrError) {
 		printf("Error!\r\n");
 		return 1;
+	}
+	
+	Binary &bin = *binOrError.get();
+	
+	if (SymbolicFile *symbol = dyn_cast<SymbolicFile>(&bin)) {
+		// ensure this is a mach-o
+		if (MachOObjectFile *macho = dyn_cast<MachOObjectFile>(symbol)) {
+			printMachoInformation(macho);
+			
+		}
+		else {
+			pritnf("What is this?\r\n");
+			return 3;
+		}
+	}
+	
+	else if (MachOUniversalBinary *macho = dyn_cast<MachOUniversalBinary>(&bin)) {
+		printf("Found mach-o universal %p\r\n", macho);
+	}
+	
+	else if (Archive *archiv = dyn_cast<Archive>(&bin)) {
+		printf("Found archive %p\r\n", archiv);
+	}
+	
+	else {
+		printf("This is not a mach-o! :(\r\n");
 	}
 	
 	return 0;
